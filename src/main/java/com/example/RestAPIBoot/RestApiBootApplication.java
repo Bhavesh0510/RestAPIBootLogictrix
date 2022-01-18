532a21b7e97d7cc630f50f554bf2e66a2e0432e9
Bhavesh0510 <bhaveshshelar0510@gmail.com>
2022-01-18 16:14:08 +0530

    New Rest API APPLICATION

---
+++ src/main/java/com/example/RestAPIBoot/RestApiBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestAPIBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestAPIBoot/RestApiBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestAPIBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
